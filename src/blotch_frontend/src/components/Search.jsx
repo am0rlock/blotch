7f61fcaa04e5d06c66223a5dd42727b54f5a3927
shreyesk <sshreyes.kaliyur@gmail.com>
2022-06-14 23:53:00 -0500

    search

--- src/blotch_frontend/src/components/Search.jsx
+++ src/blotch_frontend/src/components/Search.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import useInput from "../hooks/useInput";
+import Modal from "./Modal";
 
 import { profile_database } from '../../../declarations/profile_database/'
+import ProfilePreview from "./ProfilePreview";
 
 const InputWrapper = styled.input`
   padding: 0.4rem 0.6rem;
@@ -13,30 +14,122 @@ const InputWrapper = styled.input`
   border-radius: ${(props) => props.theme.borderRadius};
 `;
 
+const ModalWrapper = styled.div`
+	.modalContainer {
+		border: 1px solid red;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+	}
+
+	.overlay {
+		display: None;
+	}
+
+	.postContainer {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		max-width: 75vw;
+		max-height: 75vh;
+	}
+
+	.postImageContainer {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.postImage {
+		border-radius: 4px;
+		min-width: 500px;
+		min-height: 500px;
+		max-width: 500px;
+		max-height: 500px;
+		object-fit: cover;
+		padding: 5%;
+	}
+
+	textarea {
+		resize: none;
+		width: 300px;
+		margin-top: 5%;
+	}
+
+  h3 {
+    text-align: center;
+    margin: 2%;
+  }
+
+	h3:hover {
+		cursor: pointer;
+	}
+
+	.numLikers {
+	}
+
+	.comments {
+		width: 30vw;
+		height: 40vh;
+		overflow: auto;
+	}
+
+	.liked {
+		color: red;
+	}
+`;
+
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchInput: ""
+      searchInput: "",
+      showModal: false,
+      profiles: []
     }
   }
 
   handleSearch = (e) => {
     console.log(e);
     if (e.keyCode === 13) {
-      profile_database.search('hello').then(r => {
-        console.log(r);
+      profile_database.search('').then(r => {
+        this.setState({profiles: r, showModal: true}, () => {
+
+          console.log(this.state.profiles)
+        })
       });
     }
   };
 
+  hideModal = () => {
+    this.setState({showModal: false});
+  }
+
   render() {
     return (
+      <>
       <InputWrapper
         type="text"
         onKeyDown={this.handleSearch}
         placeholder="Search profiles"
       />
+			<ModalWrapper>
+				{this.state.showModal && 
+				<>
+					<div className='modalContainer'>
+						<Modal>
+              {this.state.profiles?.map((profile) => (
+                <ProfilePreview key={profile} portalPrincipal={profile}></ProfilePreview>
+              ))}
+              <h3 onClick={() => {this.hideModal()}}>Cancel</h3>
+						</Modal>
+					</div>
+				</>
+				}
+			</ModalWrapper>
+      </>
     );
   }
 };
