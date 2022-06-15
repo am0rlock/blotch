1741ae89093e299b3597d58dbb818684ac2d4da1
am0rlock <am0rlock@protonmail.com>
2022-06-14 23:32:51 -0500

    added user scripts

---
+++ tests/create_users.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dfx identity use default
+
+cd ..
+
+for i in {1..25}
+do
+    dfx identity new --disable-encryption test_$i
+    dfx identity use test_$i
+    RESPONSE=$(dfx canister call gateway grabPortal)
+done
+
+dfx identity use default
+
+for i in {1..25}
+do
+    dfx identity remove test_$i
+done
+
+cd tests/
